72826982ed210ac81abde920164fec959126b5fe
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-23 02:36:40 +0200

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Using Puppet, install flask from pip3
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin'],
-  refreshonly => true,
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
