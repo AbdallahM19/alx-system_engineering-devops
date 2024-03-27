b5b2ae7ceacbe9ea993963797d10fa5c5d4182b2
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-27 04:49:38 +0200

    web

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -18,7 +18,7 @@ file { '/var/www/html/index.html':
 }
 
 exec {'redirection':
-    command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+    command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
     provider => shell,
 }
 
