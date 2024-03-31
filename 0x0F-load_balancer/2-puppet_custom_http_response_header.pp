111f8ba2deb26e48623388dd2a52eea5d0e4b700
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-31 16:34:55 +0200

    balance

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,32 @@
 # Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
+# Nginx should be listening on port 80
+# When querying Nginx at its root / with a GET
+# request (requesting a page) using curl, it must return a page that contains the string Hello World!
+# The redirection must be a “301 Moved Permanently”
+# Your answer file should be a Puppet manifest
+# containing commands to automatically configure an Ubuntu machine to respect above requirements
 
-exec { 'update_system':
+exec { 'install_system':
     command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-    ensure  => 'present',
-    require => Exec['update_system']
+    ensure  => 'installed',
+    require => Exec['install_system']
 }
 
 file {'/var/www/html/index.html':
-	content => 'Hello World!'
+    content => 'Hello World!'
 }
 
-file_line { 'add_custom_header':
-    path     => '/etc/nginx/nginx.conf',
-    ensure   => 'present',
-    command  => 'sudo sed -i '26i\tadd_header X-Served-By "$hostname";' /etc/nginx/sites-available/default'
-    notify   => Service['nginx'],
-    require  => Package['nginx']
+exec {'redirect':
+    command  => 'sed -i "27i\	rewrite ^/redirect https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+exec { 'add_custom_header':
+    command  => 'sudo sed -i '26i\	add_header X-Served-By "$hostname";' /etc/nginx/sites-available/default'
+    provider => 'shell'
 }
 
 service { 'nginx':
