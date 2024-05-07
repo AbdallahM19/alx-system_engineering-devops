0096fe2b1b8eec52503754a1ed1845d71f7cc78b
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-05-07 01:22:18 -0700

    advancing

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API,
+returns the number of subscribers
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers else 0"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        header_Agent = {"User-Agent": "Brave version 1.65.126"}
+        x = get(url, headers=header_Agent)
+        i = x.json()
+        return i['data']['subscribers']
+    except Exception:
+        return 0
