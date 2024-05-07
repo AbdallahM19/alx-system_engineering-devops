7d7c5c54348c809ec566533f6173a5b5ae9b76ce
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-05-07 16:37:45 -0700

    advancing

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,22 +6,28 @@ listed for a given subreddit.
 """
 
 
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts else 0"""
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         print('None')
-    try:
-        url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(
-            subreddit
-        )
-        header_Agent = {"User-Agent": "Brave version 1.65.126"}
-        x = get(url, headers=header_Agent)
-        i = x.json()
-        i = i['data']['children']
-        for n in i:
-            print(n['data']['title'])
-    except Exception:
-        print('None')
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(
+        subreddit
+    )
+    header_Agent = {"User-Agent": "Brave version 1.65.126"}
+    response = requests.get(
+        url,
+        headers=header_Agent
+    )
+    data = response.json()
+    posts = data["data"]["children"]
+    i = 0
+    titles_list = []
+    for n in posts:
+        if i < 10:
+            titles_list.append("{}".format(str(n["data"]["title"])))
+            i += 1
+    for title in titles_list:
+        print(title)
