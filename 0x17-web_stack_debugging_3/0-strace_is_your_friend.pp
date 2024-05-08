1844e9dfc50c54cb0e16af041de4ae0eb026cd03
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-05-08 04:55:03 -0700

    debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # find out why Apache is returning a 500 error.
 
-exec { 'replace_line':
+exec { 'fix-wordpress':
   command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
-  path    => ['/bin', '/usr/bin']
+  path    => '/usr/local/bin/:/bin/'
 }
