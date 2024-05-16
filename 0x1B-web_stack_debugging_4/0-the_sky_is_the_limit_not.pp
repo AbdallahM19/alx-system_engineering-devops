715cd6f37f059003ace9526fa5dca7ed8324c0eb
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-05-16 13:15:03 -0700

    debug_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # we are testing how well our web server setup featuring Nginx is doing
 
-{ 'fix_nginx':
+exec { 'fix_nginx':
   provider => shell,
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
-  before  => Exec['restart_nginx']
+  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path     => '/usr/local/bin/:/bin/'
 }
 
-{ 'restart_nginx':
+exec { 'restart_nginx':
   provider => shell,
-  command => 'sudo service nginx restart'
+  command  => 'sudo service nginx restart'
 }
